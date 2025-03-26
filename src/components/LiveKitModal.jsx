0976272998f08f76c89262fead11d90f899037af
MuhammadHuzaif607 <128931955+MuhammadHuzaif607@users.noreply.github.com>
2025-03-26 20:57:37 +0500

    changes

--- src/components/LiveKitModal.jsx
+++ src/components/LiveKitModal.jsx
@@ -1,18 +1,20 @@
-import { useState, useCallback } from "react";
-import { LiveKitRoom, RoomAudioRenderer } from "@livekit/components-react";
-import "@livekit/components-styles";
-import SimpleVoiceAssistant from "./SimpleVoiceAssistant";
+import { useState, useCallback } from 'react';
+import { LiveKitRoom, RoomAudioRenderer } from '@livekit/components-react';
+import '@livekit/components-styles';
+import SimpleVoiceAssistant from './SimpleVoiceAssistant';
 
 const LiveKitModal = ({ setShowSupport }) => {
   const [isSubmittingName, setIsSubmittingName] = useState(true);
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [token, setToken] = useState(null);
 
   const getToken = useCallback(async (userName) => {
     try {
-      console.log("run")
+      console.log('run');
       const response = await fetch(
-        `/api/getToken?name=${encodeURIComponent(userName)}`
+        `https://18.205.24.243/api/getToken?name=${encodeURIComponent(
+          userName
+        )}`
       );
       const token = await response.text();
       setToken(token);
